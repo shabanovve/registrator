4b97e9ee5019e541a2f385e3db9ec50047784caa
Vladimir Shabanov <shabanovve@mail.ru>
2018-01-04 20:52:53 +0400

    Add welcome page

---
+++ backend/src/main/java/ru/yoga73/registrator/controller/PageController.java
@@ -0,0 +1,13 @@
+package ru.yoga73.registrator.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageController {
+
+    @RequestMapping("/")
+    public String getIngex() {
+        return "index";
+    }
+}
