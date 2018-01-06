91d8db9368648c4bbb05ad7ab72104bae6557154
Vladimir Shabanov <shabanovve@mail.ru>
2018-01-06 23:38:09 +0400

    Add frontend

---
+++ frontend/admin-control-panel/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+
+import {AppComponent} from './app.component';
+import {AppRoutingModule} from './/app-routing.module';
+import {FormsModule} from '@angular/forms';
+import {CustomersComponent} from './customers/customers.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CustomersComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
