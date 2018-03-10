d7e5806883ee9e5217b8a2f5250fa368153d552a
Vladimir Shabanov <shabanovve@mail.ru>
2018-03-11 00:50:18 +0400

    Add Angular Date Time Picker

--- frontend/admin-control-panel/src/app/app.module.ts
+++ frontend/admin-control-panel/src/app/app.module.ts
@@ -9,8 +9,10 @@ import {CustomersService} from './customers.service';
 import {HttpClientModule} from '@angular/common/http';
 import {LessonsComponent} from './lessons/lessons.component';
 import {LessonService} from './lesson.service';
-import { RegistrationsComponent } from './registrations/registrations.component';
+import {RegistrationsComponent} from './registrations/registrations.component';
 import {RegistrationService} from './registration.service';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {OwlDateTimeModule, OwlNativeDateTimeModule} from 'ng-pick-datetime';
 
 @NgModule({
   declarations: [
@@ -23,7 +25,10 @@ import {RegistrationService} from './registration.service';
     BrowserModule,
     FormsModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    OwlDateTimeModule,
+    OwlNativeDateTimeModule
   ],
   providers: [CustomersService, LessonService, RegistrationService],
   bootstrap: [AppComponent]
